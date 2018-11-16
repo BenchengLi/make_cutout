bdff65e586d5716d2aff9868c5dd4a105934daee
BenchengLi <42951738+BenchengLi@users.noreply.github.com>
2018-11-15 18:56:57 -0800

    Update make_cutout.py

--- make_cutout.py
+++ make_cutout.py
@@ -1,18 +1,13 @@
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
-from astropy.coordinates import SkyCoord, Galactic,FK5
 import numpy as np
 import pandas as pd
 from scipy.misc import imsave
-from scipy.misc import toimage
-import matplotlib.pyplot as plt
-import astropy.units as u
 from PIL import Image
 from scipy.misc import imshow, toimage, imsave, imread
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
-from IPython import display
 import os
 from os import walk, path
 import shutil
@@ -26,18 +21,15 @@ i=0
 n=0
 df1_list=[]
 df2_list=[]
-df_list=[]
 name_list=[]
-name_list1=[]
-fits_list=[]
 file_list=[]
 sfile_list=[]
-dir_list=[]
 path_dict=dict()
 count=1
 
 def cutout(target,radius):
     global i
+    global n
     file=fits.open(target,ignore_missing_end=True)
     file_data=file[0].data
     file_wcs=WCS(file[0].header)
@@ -45,8 +37,7 @@ def cutout(target,radius):
     num=int(len(file_data))
     x=0
     y=0
-    index=fits_list.index(target)
-    index_format='{:010d}'.format(index)
+    index_format='{:010d}'.format(n)
     try:
         os.mkdir('cutout/'+str(index_format))
     except:
@@ -75,17 +66,17 @@ def cutout(target,radius):
                 image_name='{:010d}.jpeg'.format(i)
                 imsave(image_name,im)
                 ra, dec=file_wcs.all_pix2world(x,y,0)
-                cube2=[np.int32(i),np.float32(ra),np.float32(dec),np.int32(x),np.int32(y),np.int32(index)]
+                cube2=[np.int32(i),np.float32(ra),np.float32(dec),np.int32(x),np.int32(y),np.int32(n)]
                 df2_list.append(cube2)
                 cutout_list.append(image_name)
                 shutil.move(image_name,'cutout/'+str(index_format))
-                x=x+16
+                x=x+500
                 i=i+1
             except:
-                x=x+16
+                x=x+500
         else:
             x=0
-            y=y+16
+            y=y+500
         if y>num:
             break
 
@@ -97,33 +88,23 @@ for (dirpath, dirnames, filenames) in walk(mypath):
         if '.fits' in sfile:
             name_list.append(sfile)
             path_dict[sfile]=dirpath
-            vfile=sfile[:-5]
-            for x in dir_list:
-                if vfile in x:
-                    del path_dict[sfile]
-                    name_list.remove(sfile)
     num_fits=len(name_list)
     for each_fits in path_dict:
         each_dir=each_fits[:-5]
         print ('Working on '+each_fits+' '+str(count)+'/'+str(num_fits))
-        if each_dir not in file_list:
-            path=dirpath+'/'+each_fits
-            if path not in fits_list:
-                cube1=[path,np.int32(n)]
-                df1_list.append(cube1)
-                fits_list.append(path)
-            cutout(path,ra)
-            index=fits_list.index(path)
-            index_format='{:010d}'.format(index)
-            df2=pd.DataFrame(data=df2_list,columns=['file_id','RA','Dec','x_pix','y_pix','dir_id'])
-            df2.to_pickle('./cutout/'+str(index_format)+'/big_map.pkl')
-            print(df2)
-            file_list.append(each_dir)
-            cutout_list.clear()
-            print (each_fits+' done! '+str(count)+'/'+str(num_fits))
-            count=count+1
-            n=n+1
-            df2_list.clear()
+        path=dirpath+'/'+each_fits
+        cube1=[path,np.int32(n)]
+        df1_list.append(cube1)
+        cutout(path,ra)
+        index_format='{:010d}'.format(n)
+        df2=pd.DataFrame(data=df2_list,columns=['file_id','RA','Dec','x_pix','y_pix','dir_id'])
+        df2.to_pickle('./cutout/'+str(index_format)+'/big_map.pkl')
+        print(df2)
+        cutout_list.clear()
+        print (each_fits+' done! '+str(count)+'/'+str(num_fits))
+        count=count+1
+        n=n+1
+        df2_list.clear()
     path_dict.clear()
     sfile_list.clear()
 
