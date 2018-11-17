1264168974b1c30a3cfd724a911484f0e163a183
BenchengLi <42951738+BenchengLi@users.noreply.github.com>
2018-11-17 15:56:15 -0800

    Add files via upload

---
+++ make_cutout_para.py
@@ -0,0 +1,16 @@
+def prep():
+    global i,s,m,n,mypath,ra,scale,df1_list,df2_list,name_list,file_list,sfile_list,path_dict
+    i=0
+    m=0
+    n=0
+    s=16
+    mypath='fits'
+    ra=64
+    scale=98
+    df1_list=[]
+    df2_list=[]
+    name_list=[]
+    file_list=[]
+    sfile_list=[]
+    path_dict=dict()
+    return i,s,m,n,mypath,ra,scale,df1_list,df2_list,name_list,file_list,sfile_list,path_dict
