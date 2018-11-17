724e27de9d442b4f6b9b0934940cc085106b3a6b
BenchengLi <42951738+BenchengLi@users.noreply.github.com>
2018-11-17 15:55:19 -0800

    Update make_cutout.py

--- make_cutout.py
+++ make_cutout.py
@@ -2,6 +2,7 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
 import numpy as np
+from numpy import ma, ndarray
 import pandas as pd
 from scipy.misc import imsave
 from PIL import Image
@@ -14,87 +15,80 @@ import shutil
 import pickle
 import time
 import gc
+from make_cutout_para import prep
 
-mypath=input('My path: ')
-ra=input('Radius of cutout image: ')
-scale=input('The scale(integer) of cutout: ')
-strid=input('The stride: ')
 start_time=time.time()
-i=0
-m=0
-n=0
-x=-1
-df1_list=[]
-df2_list=[]
-name_list=[]
-file_list=[]
-sfile_list=[]
-path_dict=dict()
+i,s,m,n,mypath,ra,scale,df1_list,df2_list,name_list,file_list,sfile_list,path_dict = prep()
 
+#Open a FITS file, read its data and create its corresponding folder
+def open_fits(target):
+    global file_data, file_wcs, num_x, num_y
+    file=fits.open(target,ignore_missing_end=True)
+    file_data=file[0].data
+    file_wcs=WCS(file[0].header)
+    file_wcs.sip=None
+    num_x=int(np.ma.size(file_data,1))
+    num_y=int(np.ma.size(file_data,0))
+
+#Get the x,y boundary for a FITS file
+def get_boundary(num1,num2):
+    global x_list, y_list
+    x_list=[]
+    y_list=[]
+    for n in range(int(num1/s)+1):
+        if num1-n>0.5*s:
+            x_list.append(int(n*s))
+    for n in range(int(num2/s)+1):
+        if num2-n>0.5*s:
+            y_list.append(int(n*s))
+
+#Prepare the data for cutout process
+def normal_data(data):
+    global data3,data_max,nan_count,data3_max
+    flat_data=np.ravel(data)
+    flat_data = flat_data[np.logical_not(np.isnan(flat_data))]
+    data_max=np.percentile(flat_data,int(scale))
+    data1=np.maximum(data,0)
+    data2=np.minimum(data1, data_max)
+    data3=data2/data_max*255
+    flat_data3=np.ravel(data3)
+    data3_max=np.percentile(flat_data3,30)
+
+#save data array into jpeg images
+def save(data):
+    im=Image.fromarray(data)
+    if im.mode != 'RGB':
+        im=im.convert('RGB')
+    image_name='{:010d}.jpeg'.format(i)
+    imsave(image_name,im)
+    ra, dec=file_wcs.all_pix2world(x,y,0)
+    cube2=[np.int32(i),np.float32(ra),np.float32(dec),np.int32(x),np.int32(y),np.float32(data_max),np.int32(n)]
+    df2_list.append(cube2)
+    shutil.move(image_name,'cutout/'+str(index_format))
+
+#cut FITS file
 def cutout(target,radius):
-    global i
-    global n
-    global x
-    global m
-    try:
-        file=fits.open(target,ignore_missing_end=True)
-        file_data=file[0].data
-        file_wcs=WCS(file[0].header)
-        file_wcs.sip=None
-        num=int(len(file_data))
-        index_format='{:010d}'.format(n)
-        try:
-            os.mkdir('cutout/'+str(index_format))
-        except:
-            print('Folder '+str(index_format)+' already exists!')
-        x=0
-        y=0
-    except:
-        print('Cannot open '+str(target))
-        x=-1
-    while True:
-        if x==-1:
-            break
-        if x <= num:
-            try:
-                cutout=Cutout2D(data=file_data, position=(x,y), size=int(radius), wcs=file_wcs,mode='partial')
-                data=cutout.data
-                flat_data=data.ravel()
-                flat_data = flat_data[np.logical_not(np.isnan(flat_data))]
-                data_min=np.percentile(flat_data,100-int(scale))
-                data_max=np.percentile(flat_data,int(scale))
-                if data_min < 0:
-                    data=data-data_min
-                data1=np.maximum(data,0)
-                data2=np.minimum(data1,data_max)
-                data_std=np.std(data2)
-                if data_std<1:
-                    data_thre=np.percentile(data2,60)
-                    data2=(data2-data_thre)*1/data_std+data_thre
-                    data_max=np.percentile(data2,int(scale))
-                data3=data2/data_max*255
-                flat_data3=data3.ravel()
-                data3_max=np.percentile(flat_data3,90)
-                if np.isnan(data3_max)==False:
-                    im=Image.fromarray(data3)
-                    if im.mode != 'RGB':
-                        im=im.convert('RGB')
-                    image_name='{:010d}.jpeg'.format(i)
-                    imsave(image_name,im)
-                    ra, dec=file_wcs.all_pix2world(x,y,0)
-                    cube2=[np.int32(i),np.float32(ra),np.float32(dec),np.int32(x),np.int32(y),np.float32(data_max),np.int32(n)]
-                    df2_list.append(cube2)
-                    shutil.move(image_name,'cutout/'+str(index_format))
-                    i=i+1
-                x=x+int(strid)
-            except:
-                x=x+int(strid)
-        else:
-            x=0
-            y=y+int(strid)
-        if y>num:
-            break
+    global i, m,n,x,y,index_format
+    open_fits(target)
+    get_boundary(num_x,num_y)
+    index_format='{:010d}'.format(n)
+    folder_name='cutout/'+str(index_format)
+    if os.path.exists(folder_name)==False:
+        os.mkdir(folder_name)
+    else:
+        print(folder_name+' already exists!')
+    # loop
+    for y in y_list:
+        for x in x_list:
+            cutout=Cutout2D(data=file_data, position=(x,y), size=int(radius), wcs=file_wcs,mode='partial')
+            data=cutout.data
+            normal_data(data)
+            if np.isnan(data3_max)==False:
+                save(data3)
+                i=i+1
 
+
+# Scan through the directory looking for FITS file
 for (dirpath, dirnames, filenames) in walk(mypath):
     for (sdirpath, sdirnames, sfilenames) in walk(dirpath):
         sfile_list=sfilenames
@@ -129,4 +123,3 @@ df1=pd.DataFrame(data=df1_list,columns=['parent directory','dir_id'])
 df1.to_pickle("./cutout/directory_map.pkl")
 print (df1)
 print("--- %s seconds ---" % (time.time() - start_time))
-
