533b5370571ddc117a8c84ac8dde25185f7c549d
BenchengLi <42951738+BenchengLi@users.noreply.github.com>
2018-12-06 16:26:48 -0800

    Update make_cutout_para.py

--- make_cutout_para.py
+++ make_cutout_para.py
@@ -1,14 +1,6 @@
 def prep():
-    global i,s,m,n,mypath,ra,scale,df1_list,df2_list,name_list,path_dict
-    i=0
-    m=0
-    n=0
-    s=16
-    mypath='fits'
-    ra=64
-    scale=98
-    df1_list=[]
-    df2_list=[]
-    name_list=[]
-    path_dict=dict()
-    return i,s,m,n,mypath,ra,scale,df1_list,df2_list,name_list,path_dict
+
+    s=50
+    data_dir='fits'
+    save_dir='cutout'
+    return s, data_dir, save_dir
